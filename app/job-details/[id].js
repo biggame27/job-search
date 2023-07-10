22277513032ee95df9cb1ef60c763b064d219fb4
Harrison <koharrisonko@gmail.com>
2023-07-09 20:48:53 -0500

    finished project

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -18,8 +18,30 @@ const JobDetails = () => {
     })    
 
     const [refreshing, setRefreshing] = useState(false)
+    const [activeTab, setActiveTab] = useState(tabs[0])
 
     const onRefresh = () => {}
+
+    const displayTabContent = () => {
+        switch (activeTab) {
+            case "Qualifications":
+                return <Specifics 
+                    title="Qualifications"
+                    points={data[0].job_highlights?.Qualifications ?? ['N/A']}
+                />
+            case "About":
+                return <JobAbout 
+                    info={data[0].job_description ?? "No data provided"}
+                />
+            case "Responsibilities":
+                return <Specifics 
+                    title="Responsibilities"
+                    points={data[0].job_highlights?.Responsibilities ?? ['N/A']}
+                />
+            default:
+                break;
+        }
+    }
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -66,11 +88,17 @@ const JobDetails = () => {
                             />
 
                             <JobTabs 
-                            
+                                tabs={tabs}
+                                activeTab={activeTab}
+                                setActiveTab={setActiveTab}
                             />
+
+                            {displayTabContent()}
                         </View>
                     ) }
                 </ScrollView>
+
+                <JobFooter url={data[0]?.job_google_link ?? 'https://careers.google.com/jobs/results'} />
             </>
         </SafeAreaView>
     )
